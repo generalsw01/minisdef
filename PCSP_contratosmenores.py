96fdabed3032b40c492f279e3835a89a67033a7f
egorusa <eduardogozaloruiz@egorusa.es>
2021-07-14 14:12:41 +0200

    20210714 v.1.3

--- PCSP_contratosmenores.py
+++ PCSP_contratosmenores.py
@@ -1112,23 +1112,6 @@ def fc_entries(df_licitaciones,entries_to_normalize,link_self):
     return(dict_dfs)
 
 
-# In[7]:
-
-
-def test_connector(config):
-    try:
-      cnx = mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-      elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-      else:
-        print(err)
-    else:
-      cnx.close()
-test_connector(cnxn_string)    
-
 
 # In[8]:
 
