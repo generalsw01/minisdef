b60d08458f9c35f5bbda01723cc2cfebaafac6ad
egorusa <eduardogozaloruiz@egorusa.es>
2021-07-14 19:26:28 +0200

    20210714 v.1

--- Start.py
+++ Start.py
@@ -3,12 +3,12 @@
 
 
 
-_server = '10.20.50.42'
+_server = '195.57.30.196'
 #_server = 'vidreo.ddns.net'
 _database = 'portal_contratacion'
 _username = 'portal_contrataciondba'
 _password = 'Tm@s8+En-z..}'
-_port='3306'
+_port='20043'
 
 engine_string = 'mysql+mysqlconnector://'+_username+':'+_password+'@'+_server+':'+_port+'/'+_database
 cnxn_string = {
