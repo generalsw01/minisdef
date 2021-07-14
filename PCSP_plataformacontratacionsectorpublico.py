1b582d3dbda40529e83a43cd753ea84299a3a2eb
egorusa <eduardogozaloruiz@egorusa.es>
2021-07-14 19:13:07 +0200

    20210714 v.2.1

--- PCSP_plataformacontratacionsectorpublico.py
+++ PCSP_plataformacontratacionsectorpublico.py
@@ -16,12 +16,12 @@ from datetime import datetime
 import mysql.connector
 from mysql.connector import errorcode
 
-_server = '195.57.30.196'
+_server = '10.20.50.42'
 #_server = 'vidreo.ddns.net'
 _database = 'portal_contratacion'
 _username = 'portal_contrataciondba'
 _password = 'Tm@s8+En-z..}'
-_port='20043'
+_port='3306'
 
 engine_string = 'mysql+mysqlconnector://'+_username+':'+_password+'@'+_server+':'+_port+'/'+_database
 cnxn_string = {
